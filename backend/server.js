f365b02268ffa0c41c26512858130f94d5f8cf7b
ABHISHEK SUDHIR <abhisheksudhir2000@gmail.com>
2020-11-14 16:25:43 +0530

    connecting frontend to backend

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,20 @@ app.use(express.json()); //to parse json
 //connecting to database
 connectDB();
 
+// CORS HEADERS MIDDLEWARE
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-access-token, x-refresh-token, _id");
+
+  res.header(
+      'Access-Control-Expose-Headers',
+      'x-access-token, x-refresh-token'
+  );
+
+  next();
+});
+
 // Load in the mongoose models
 const { List, Task } = require("./db/models");
 
